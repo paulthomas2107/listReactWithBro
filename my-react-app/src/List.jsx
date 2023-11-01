f6d3e5377fa989cfdaa16062bbe152c6e6b8b67f
Paul Thomas <paulthomas2107@gmail.com>
2023-11-01 09:35:46 +0000

    xx

---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,9 @@
+function List() {
+  const fruits = ['apple', 'orange', 'banana', 'coconut', 'pineapple'];
+
+  const listItems = fruits.map((fruit) => <li>{fruit}</li>);
+
+  return <ol>{listItems}</ol>;
+}
+
+export default List;
